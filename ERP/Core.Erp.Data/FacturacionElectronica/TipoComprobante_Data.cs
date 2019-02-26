de094928b2b7588fd5f74f2e3c0cccd5e2aea26f
carloscede <carloscede@hotmail.es>
2019-02-26 12:11:36 -0800

    ....

---
+++ ERP/Core.Erp.Data/FacturacionElectronica/TipoComprobante_Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Erp.Info.FacturacionElectronica.Factura_V2;
+using Core.Erp.Info.FacturacionElectronica.GuiaRemision;
+using Core.Erp.Info.FacturacionElectronica.NotaCredito;
+using Core.Erp.Info.FacturacionElectronica.NotaDebito;
+using Core.Erp.Info.FacturacionElectronica.Retencion;
+using Core.Erp.Info.FacturacionElectronica;
+
+namespace Core.Erp.Data.FacturacionElectronica
+{
+  public  class TipoComprobante_Data
+    {
+       
+
+        public List<TipoComprobante_Info> get_list(DateTime FechaInicio, DateTime FechaFin)
+        {
+            try
+            {
+                List<TipoComprobante_Info> listado_documentos = new List<TipoComprobante_Info>();
+
+
+                #region Facturas Fixed
+
+                #endregion
+
+
+                return listado_documentos;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
