c590c70184e381f6fbb7b1d8944bc6d01cd698ec
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-02-27 14:35:27 -0500

    ;poijhgv

---
+++ ERP/Core.Erp.Bus/Reportes/CuentasPorPagar/CXP_014_Bus.cs
@@ -0,0 +1,28 @@
+﻿using Core.Erp.Data.Reportes.CuentasPorPagar;
+using Core.Erp.Info.Reportes.CuentasPorPagar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Reportes.CuentasPorPagar
+{
+    public class CXP_014_Bus
+    {
+        CXP_014_Data odata = new CXP_014_Data();
+         public List<CXP_014_Info> GetList(int IdEmpresa, int IdSucursal, decimal IdProveedor, DateTime fecha_ini, DateTime fecha_fin)
+        {
+            try
+            {
+                return odata.GetList(IdEmpresa, IdSucursal, IdProveedor, fecha_ini, fecha_fin);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
