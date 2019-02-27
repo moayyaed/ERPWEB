2534fabf87c3a0ab9c870b0936abf675207c19d4
carloscede <carloscede@hotmail.es>
2019-02-27 12:30:22 -0800

    ....

--- ERP/Core.Erp.Data/Reportes/RRHH/ROL_025_Data.cs
+++ ERP/Core.Erp.Data/Reportes/RRHH/ROL_025_Data.cs
@@ -9,7 +9,7 @@ namespace Core.Erp.Data.Reportes.RRHH
 {
     public class ROL_025_Data
     {
-        public List<ROL_021_Info> get_list(int IdEmpresa, int IdSucursal, int IdNominaTipo, int IdPeriodo)
+        public List<ROL_025_Info> get_list(int IdEmpresa, int IdSucursal, int IdNominaTipo, int IdPeriodo)
         {
             try
             {
@@ -18,21 +18,21 @@ namespace Core.Erp.Data.Reportes.RRHH
 
 
                 List<ROL_025_Info> Lista = new List<ROL_025_Info>();
-                using (Entities_reportes Context = new Entities_reportes())
-                {                                     
-                        Lista = (from q in Context.SPROL_025
-                                 where q.IdEmpresa == IdEmpresa
-                                 && q.IdPeriodo == IdPeriodo
-                                 && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
-                                 && q.IdNominaTipoLiqui == IdNominaTipo
-                                 select new ROL_025_Info
-                                 {
-                                     IdEmpresa = q.IdEmpresa,
-                                     IdRol = q.IdRol,
-                                     IdSucursal = q.IdSucursal,
-                                     IdNominaTipo = q.IdNominaTipo,
-                                 }).ToList();
-                }
+                //using (Entities_reportes Context = new Entities_reportes())
+                //{                                     
+                //        Lista = (from q in Context.SPROL_025
+                //                 where q.IdEmpresa == IdEmpresa
+                //                 && q.IdPeriodo == IdPeriodo
+                //                 && IdSucursalInicio <= q.IdSucursal && q.IdSucursal <= IdSucursalFin
+                //                 && q.IdNominaTipoLiqui == IdNominaTipo
+                //                 select new ROL_025_Info
+                //                 {
+                //                     IdEmpresa = q.IdEmpresa,
+                //                     IdRol = q.IdRol,
+                //                     IdSucursal = q.IdSucursal,
+                //                     IdNominaTipo = q.IdNominaTipo,
+                //                 }).ToList();
+                //}
                 return Lista;
             }
             catch (Exception)
