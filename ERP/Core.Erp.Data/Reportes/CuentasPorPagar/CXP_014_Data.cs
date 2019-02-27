a4692a2fff04991de2691b3f86b141a53c2d5ba4
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-02-27 12:32:58 -0500

    s

--- ERP/Core.Erp.Data/Reportes/CuentasPorPagar/CXP_014_Data.cs
+++ ERP/Core.Erp.Data/Reportes/CuentasPorPagar/CXP_014_Data.cs
@@ -9,15 +9,23 @@ namespace Core.Erp.Data.Reportes.CuentasPorPagar
 {
   public class CXP_014_Data
     {
-         public List<CXP_014_Info> GetList()
+         public List<CXP_014_Info> GetList(int IdEmpresa,int IdSucursal, decimal IdProveedor, DateTime fecha_ini, DateTime fecha_fin)
+
         {
             try
             {
                 List<CXP_014_Info> Lista;
+                decimal IdProveedorIni = IdProveedor;
+                decimal IdProveedorFin = IdProveedor == 0 ? 9999 : IdProveedor;
                 using (Entities_reportes Context = new Entities_reportes())
                 {
-                    Lista = Context.VWCXP_014.Select(q => new CXP_014_Info
-
+                    Lista = Context.VWCXP_014.Where (q=> q.IdEmpresa == IdEmpresa
+                    && q.IdSucursal == IdSucursal
+                    && IdProveedorIni <= q.IdProveedor
+                    && q.IdProveedor <= IdProveedorFin
+                    && fecha_ini <= q.co_FechaFactura
+                    && q.co_FechaFactura <= fecha_fin
+                    ).Select(q => new CXP_014_Info
                     {
 
                     }).ToList();
