072e03c20d4c1509a61f59e682fb7ea6436d7a97
carloscede <carloscede@hotmail.es>
2019-03-06 23:47:04 -0800

    .....

---
+++ ERP/Core.Erp.Bus/FacturacionElectronica/TipoComprobante_Bus.cs
@@ -0,0 +1,28 @@
+﻿using Core.Erp.Data.FacturacionElectronica;
+using Core.Erp.Info.FacturacionElectronica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.FacturacionElectronica
+{
+   public class TipoComprobante_Bus
+    {
+
+        TipoComprobante_Data odata = new TipoComprobante_Data();
+        public TipoComprobante_Info get_list_comprobantes(DateTime FechaInicio, DateTime FechaFin)
+        {
+            try
+            {
+                return odata.get_list_comprobantes(FechaInicio, FechaFin);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        }
+}
