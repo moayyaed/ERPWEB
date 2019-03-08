bd32aa8663886a08a382530f67de7de2f4a49415
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-03-08 10:32:07 -0500

    iop

--- ERP/Core.Erp.Bus/Reportes/RRHH/ROL_013_Bus.cs
+++ ERP/Core.Erp.Bus/Reportes/RRHH/ROL_013_Bus.cs
@@ -8,12 +8,12 @@ namespace Core.Erp.Bus.Reportes.RRHH
     public class ROL_013_Bus
     {
         ROL_013_Data odata = new ROL_013_Data();
-    
-        public List<ROL_013_Info> get_list(int IdEmpresa, int IdNomina, decimal IdEmpleado,  DateTime fecha_inicio, DateTime fecha_fin)
+
+        public List<ROL_013_Info> get_list(int IdEmpresa, int IdNomina, int IdSucursal, int IdPeriodo)
         {
             try
             {
-                return odata.get_list(IdEmpresa, IdNomina, IdEmpleado, fecha_inicio, fecha_fin);
+                return odata.get_list(IdEmpresa, IdNomina, IdSucursal, IdPeriodo);
             }
             catch (Exception)
             {
