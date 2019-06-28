41c1f460aabf8b19189fb74639ae3b0491cf744d
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-06-28 08:13:39 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Stored Procedures/SPINV_GetStock.sql
@@ -0,0 +1,24 @@
+﻿--exec SPINV_GetStock 2,1,1,72,'2019/12/01'
+CREATE PROCEDURE SPINV_GetStock
+(
+@IdEmpresa int,
+@IdSucursal int,
+@IdBodega int,
+@IdProducto numeric,
+@FechaCorte date
+)
+AS
+
+DECLARE @UltCosto float, @IdPeriodo int, @IdFecha int
+
+SET @IdPeriodo = cast( cast(year(@FechaCorte) as varchar(4)) + right('00'+ cast(month(@FechaCorte) as varchar(2)),2)+ right('00'+ cast(day(@FechaCorte) as varchar(2)),2) as int)
+select top 1 @UltCosto = c.costo, @IdFecha = IdFecha
+from in_producto_x_tb_bodega_Costo_Historico as c 
+where c.IdEmpresa = @IdEmpresa and c.IdSucursal = @IdSucursal and c.IdBodega = @IdBodega and c.IdProducto = @IdProducto and c.IdFecha <= @IdPeriodo
+order by c.IdEmpresa, c.IdSucursal, c.IdBodega, c.IdProducto, c.IdFecha desc
+
+SELECT d.IdEmpresa, d.IdSucursal, d.IdBodega, d.IdProducto, d.IdUnidadMedida,ISNULL(sum(dm_cantidad),0) Stock, ISNULL(@UltCosto,0) UltimoCosto, isnull(@IdFecha,0) IdFecha
+FROM in_movi_inve_detalle AS D INNER JOIN
+in_movi_inve AS C ON C.IdEmpresa = D.IdEmpresa AND C.IdSucursal = D.IdSucursal AND C.IdBodega = D.IdBodega AND C.IdMovi_inven_tipo = D.IdMovi_inven_tipo AND C.IdNumMovi = D.IdNumMovi
+WHERE D.IdEmpresa = @IdEmpresa AND D.IdSucursal = @IdSucursal AND D.IdBodega = @IdBodega AND D.IdProducto = @IdProducto and c.cm_fecha <= @FechaCorte
+group by d.IdEmpresa, d.IdSucursal, d.IdBodega, d.IdProducto, d.IdUnidadMedida
--- ERP_database/ERP_database/web/Views/VWINV_018.sql
+++ ERP_database/ERP_database/web/Views/VWINV_018.sql
@@ -1,18 +1,238 @@
 ﻿CREATE VIEW web.VWINV_018
 AS
-SELECT in_AjusteDet.IdEmpresa, in_AjusteDet.IdAjuste,in_AjusteDet.Secuencia, in_Ajuste.IdSucursal, in_Ajuste.IdBodega, in_Ajuste.IdMovi_inven_tipo_ing, in_Ajuste.IdMovi_inven_tipo_egr, in_Ajuste.IdNumMovi_ing, in_Ajuste.IdNumMovi_egr, in_Ajuste.IdCatalogo_Estado, 
-                  in_Ajuste.Estado, in_Ajuste.Fecha, in_Ajuste.Observacion, tb_sucursal.Su_Descripcion, tb_bodega.bo_Descripcion, movi_ing.tm_descripcion AS tm_descripcion_ing, movi_egr.tm_descripcion AS tm_descripcion_egr, 
-                  in_AjusteDet.IdProducto, in_AjusteDet.IdUnidadMedida, in_AjusteDet.StockSistema, in_AjusteDet.StockFisico, in_AjusteDet.Ajuste, in_AjusteDet.Costo, in_Producto.pr_descripcion, in_UnidadMedida.Descripcion AS NomUnidadMedida, 
-                  in_categorias.ca_Categoria, in_linea.nom_linea
-FROM     in_UnidadMedida INNER JOIN
-                  in_Ajuste INNER JOIN
-                  in_AjusteDet ON in_Ajuste.IdEmpresa = in_AjusteDet.IdEmpresa AND in_Ajuste.IdAjuste = in_AjusteDet.IdAjuste INNER JOIN
-                  tb_bodega ON in_Ajuste.IdEmpresa = tb_bodega.IdEmpresa AND in_Ajuste.IdEmpresa = tb_bodega.IdEmpresa AND in_Ajuste.IdSucursal = tb_bodega.IdSucursal AND in_Ajuste.IdSucursal = tb_bodega.IdSucursal AND 
-                  in_Ajuste.IdBodega = tb_bodega.IdBodega AND in_Ajuste.IdBodega = tb_bodega.IdBodega INNER JOIN
-                  tb_sucursal ON tb_bodega.IdEmpresa = tb_sucursal.IdEmpresa AND tb_bodega.IdSucursal = tb_sucursal.IdSucursal INNER JOIN
-                  in_Producto ON in_AjusteDet.IdEmpresa = in_Producto.IdEmpresa AND in_AjusteDet.IdProducto = in_Producto.IdProducto INNER JOIN
-                  in_linea INNER JOIN
-                  in_categorias ON in_linea.IdEmpresa = in_categorias.IdEmpresa AND in_linea.IdCategoria = in_categorias.IdCategoria ON in_Producto.IdEmpresa = in_linea.IdEmpresa AND in_Producto.IdCategoria = in_linea.IdCategoria AND 
-                  in_Producto.IdLinea = in_linea.IdLinea ON in_UnidadMedida.IdUnidadMedida = in_AjusteDet.IdUnidadMedida INNER JOIN
-                  in_movi_inven_tipo AS movi_ing ON in_Ajuste.IdEmpresa = movi_ing.IdEmpresa AND in_Ajuste.IdMovi_inven_tipo_ing = movi_ing.IdMovi_inven_tipo INNER JOIN
-                  in_movi_inven_tipo AS movi_egr ON in_Ajuste.IdEmpresa = movi_egr.IdEmpresa AND in_Ajuste.IdMovi_inven_tipo_egr = movi_egr.IdMovi_inven_tipo
+SELECT dbo.in_AjusteDet.IdEmpresa, dbo.in_AjusteDet.IdAjuste, dbo.in_AjusteDet.Secuencia, dbo.in_Ajuste.IdSucursal, dbo.in_Ajuste.IdBodega, dbo.in_Ajuste.IdMovi_inven_tipo_ing, dbo.in_Ajuste.IdMovi_inven_tipo_egr, 
+                  dbo.in_Ajuste.IdNumMovi_ing, dbo.in_Ajuste.IdNumMovi_egr, dbo.in_Ajuste.IdCatalogo_Estado, dbo.in_Ajuste.Estado, dbo.in_Ajuste.Fecha, dbo.in_Ajuste.Observacion, dbo.tb_sucursal.Su_Descripcion, dbo.tb_bodega.bo_Descripcion, 
+                  movi_ing.tm_descripcion AS tm_descripcion_ing, movi_egr.tm_descripcion AS tm_descripcion_egr, dbo.in_AjusteDet.IdProducto, dbo.in_AjusteDet.IdUnidadMedida, dbo.in_AjusteDet.StockSistema, dbo.in_AjusteDet.StockFisico, 
+                  dbo.in_AjusteDet.Ajuste, dbo.in_AjusteDet.Costo, dbo.in_Producto.pr_descripcion, dbo.in_UnidadMedida.Descripcion AS NomUnidadMedida, dbo.in_categorias.ca_Categoria, dbo.in_linea.nom_linea, 
+                  dbo.in_AjusteDet.Ajuste * dbo.in_AjusteDet.Costo AS Total, dbo.in_Catalogo.Nombre AS NombreEstado
+FROM     dbo.in_UnidadMedida INNER JOIN
+                  dbo.in_Ajuste INNER JOIN
+                  dbo.in_AjusteDet ON dbo.in_Ajuste.IdEmpresa = dbo.in_AjusteDet.IdEmpresa AND dbo.in_Ajuste.IdAjuste = dbo.in_AjusteDet.IdAjuste INNER JOIN
+                  dbo.tb_bodega ON dbo.in_Ajuste.IdEmpresa = dbo.tb_bodega.IdEmpresa AND dbo.in_Ajuste.IdEmpresa = dbo.tb_bodega.IdEmpresa AND dbo.in_Ajuste.IdSucursal = dbo.tb_bodega.IdSucursal AND 
+                  dbo.in_Ajuste.IdSucursal = dbo.tb_bodega.IdSucursal AND dbo.in_Ajuste.IdBodega = dbo.tb_bodega.IdBodega AND dbo.in_Ajuste.IdBodega = dbo.tb_bodega.IdBodega INNER JOIN
+                  dbo.tb_sucursal ON dbo.tb_bodega.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.tb_bodega.IdSucursal = dbo.tb_sucursal.IdSucursal INNER JOIN
+                  dbo.in_Producto ON dbo.in_AjusteDet.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_AjusteDet.IdProducto = dbo.in_Producto.IdProducto INNER JOIN
+                  dbo.in_linea INNER JOIN
+                  dbo.in_categorias ON dbo.in_linea.IdEmpresa = dbo.in_categorias.IdEmpresa AND dbo.in_linea.IdCategoria = dbo.in_categorias.IdCategoria ON dbo.in_Producto.IdEmpresa = dbo.in_linea.IdEmpresa AND 
+                  dbo.in_Producto.IdCategoria = dbo.in_linea.IdCategoria AND dbo.in_Producto.IdLinea = dbo.in_linea.IdLinea ON dbo.in_UnidadMedida.IdUnidadMedida = dbo.in_AjusteDet.IdUnidadMedida INNER JOIN
+                  dbo.in_Catalogo ON dbo.in_Ajuste.IdCatalogo_Estado = dbo.in_Catalogo.IdCatalogo LEFT OUTER JOIN
+                  dbo.in_movi_inven_tipo AS movi_ing ON dbo.in_Ajuste.IdEmpresa = movi_ing.IdEmpresa AND dbo.in_Ajuste.IdMovi_inven_tipo_ing = movi_ing.IdMovi_inven_tipo LEFT OUTER JOIN
+                  dbo.in_movi_inven_tipo AS movi_egr ON dbo.in_Ajuste.IdEmpresa = movi_egr.IdEmpresa AND dbo.in_Ajuste.IdMovi_inven_tipo_egr = movi_egr.IdMovi_inven_tipo
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 2, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWINV_018';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane2', @value = N'            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_categorias"
+            Begin Extent = 
+               Top = 1183
+               Left = 48
+               Bottom = 1346
+               Right = 257
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "movi_ing"
+            Begin Extent = 
+               Top = 1351
+               Left = 48
+               Bottom = 1514
+               Right = 303
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "movi_egr"
+            Begin Extent = 
+               Top = 1519
+               Left = 48
+               Bottom = 1682
+               Right = 303
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_Catalogo"
+            Begin Extent = 
+               Top = 1521
+               Left = 351
+               Bottom = 1684
+               Right = 559
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWINV_018';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = -1514
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "in_UnidadMedida"
+            Begin Extent = 
+               Top = 7
+               Left = 48
+               Bottom = 170
+               Right = 256
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_Ajuste"
+            Begin Extent = 
+               Top = 175
+               Left = 48
+               Bottom = 338
+               Right = 293
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_AjusteDet"
+            Begin Extent = 
+               Top = 343
+               Left = 48
+               Bottom = 506
+               Right = 255
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_bodega"
+            Begin Extent = 
+               Top = 511
+               Left = 48
+               Bottom = 674
+               Right = 360
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_sucursal"
+            Begin Extent = 
+               Top = 679
+               Left = 48
+               Bottom = 842
+               Right = 320
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_Producto"
+            Begin Extent = 
+               Top = 847
+               Left = 48
+               Bottom = 1010
+               Right = 323
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "in_linea"
+            Begin Extent = 
+               Top = 1015
+               Left = 48
+               Bottom = 1178
+               Right = 256
+            End
+', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWINV_018';
+
