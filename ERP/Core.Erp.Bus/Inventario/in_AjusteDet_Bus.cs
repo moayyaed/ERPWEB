6637a76bda4c3593fb84287fd87faa308adb617e
WendyCherres <wendycherres@gmail.com>
2019-06-28 19:31:16 -0500

    ajuste fisico

---
+++ ERP/Core.Erp.Bus/Inventario/in_AjusteDet_Bus.cs
@@ -0,0 +1,40 @@
+﻿using Core.Erp.Data.Inventario;
+using Core.Erp.Info.Inventario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Inventario
+{
+    public class in_AjusteDet_Bus
+    {
+        in_AjusteDet_Data odata = new in_AjusteDet_Data();
+        public List<in_AjusteDet_Info> get_list(int IdEmpresa, decimal IdAjuste)
+        {
+            try
+            {
+                return odata.GetList(IdEmpresa, IdAjuste);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<in_AjusteDet_Info> get_list_cargar_detalle(DateTime Fecha, int IdEmpresa = 0, int IdSucursal = 0, int IdBodega = 0)
+        {
+            try
+            {
+                return odata.get_list_cargar_detalle(IdEmpresa, IdSucursal, IdBodega, Fecha);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
