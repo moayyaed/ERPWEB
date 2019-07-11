6770831c98fc91c421ee5705444ea28585008caa
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-07-11 09:44:56 -0500

    mia,mia,mia

---
+++ ERP/Core.Erp.Info/Reportes/Contabilidad/CONTA_006_Info.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Reportes.Contabilidad
+{
+    public class CONTA_006_Info
+    {
+        public string IdUsuario { get; set; }
+        public int IdEmpresa { get; set; }
+        public string IdCtaCble { get; set; }
+        public string pc_Cuenta { get; set; }
+        public string IdCtaCblePadre { get; set; }
+        public bool EsCtaUtilidad { get; set; }
+        public int IdNivelCta { get; set; }
+        public string IdGrupoCble { get; set; }
+        public string gc_GrupoCble { get; set; }
+        public string gc_estado_financiero { get; set; }
+        public int gc_Orden { get; set; }
+        public bool EsCuentaMovimiento { get; set; }
+        public string Naturaleza { get; set; }
+        public decimal Enero { get; set; }
+        public decimal Febrero { get; set; }
+        public decimal Marzo { get; set; }
+        public decimal Abril { get; set; }
+        public decimal Mayo { get; set; }
+        public decimal Junio { get; set; }
+        public decimal Julio { get; set; }
+        public decimal Agosto { get; set; }
+        public decimal Septiembre { get; set; }
+        public decimal Octubre { get; set; }
+        public decimal Noviembre { get; set; }
+        public decimal Diciembre { get; set; }
+        public decimal Total { get; set; }
+    }
+}
