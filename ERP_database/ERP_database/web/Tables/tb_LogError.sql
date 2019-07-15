b05053ce779c29f718c44797656cc92ddc4b7cc6
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2019-07-15 16:01:21 -0500

    ojalaquelluevacafe

--- ERP_database/ERP_database/web/Tables/tb_LogError.sql
+++ ERP_database/ERP_database/web/Tables/tb_LogError.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [web].[tb_LogError] (
-    [IdError]     NUMERIC (18)  IDENTITY (1, 1) NOT NULL,
-    [Fecha]       DATETIME      NOT NULL,
-    [Descripcion] VARCHAR (MAX) NULL,
-    [IdUsuario]   VARCHAR (50)  NULL,
-    [Clase]   VARCHAR (1000) NULL,
-    [Metodo] VARCHAR(1000) NULL, 
+    [IdError]        NUMERIC (18)   IDENTITY (1, 1) NOT NULL,
+    [Fecha]          DATETIME       NOT NULL,
+    [Descripcion]    VARCHAR (MAX)  NULL,
+    [IdUsuario]      VARCHAR (50)   NULL,
+    [Clase]          VARCHAR (1000) NULL,
+    [Metodo]         VARCHAR (1000) NULL,
+    [InnerException] VARCHAR (MAX)  NULL,
     CONSTRAINT [PK_tb_LogError] PRIMARY KEY CLUSTERED ([IdError] ASC)
 );
+
+
 
