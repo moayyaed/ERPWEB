fa9cedea4b541a215835189c3d7b6aebbd815150
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-07-25 13:43:32 -0500

    y

--- ERP_database/ERP_database/web/Views/vwfa_factura_det.sql
+++ ERP_database/ERP_database/web/Views/vwfa_factura_det.sql
@@ -2,8 +2,8 @@
 AS
 SELECT        dbo.fa_factura_det.IdEmpresa, dbo.fa_factura_det.IdSucursal, dbo.fa_factura_det.IdBodega, dbo.fa_factura_det.IdCbteVta, dbo.fa_factura_det.Secuencia, dbo.fa_factura_det.IdProducto, dbo.fa_factura_det.vt_cantidad, 
                          dbo.fa_factura_det.vt_Precio, dbo.fa_factura_det.vt_PorDescUnitario, dbo.fa_factura_det.vt_DescUnitario, dbo.fa_factura_det.vt_PrecioFinal, dbo.fa_factura_det.vt_Subtotal, dbo.fa_factura_det.vt_iva, dbo.fa_factura_det.vt_total, 
-                         dbo.fa_factura_det.vt_estado, dbo.fa_factura_det.vt_detallexItems, dbo.fa_factura_det.vt_por_iva, dbo.fa_factura_det.IdPunto_Cargo, dbo.fa_factura_det.IdPunto_cargo_grupo, dbo.fa_factura_det.IdCod_Impuesto_Iva, 
-                         dbo.fa_factura_det.IdCod_Impuesto_Ice, dbo.fa_factura_det.IdCentroCosto, dbo.fa_factura_det.IdCentroCosto_sub_centro_costo, dbo.fa_factura_det.IdEmpresa_pf, dbo.fa_factura_det.IdSucursal_pf, 
+                          dbo.fa_factura_det.vt_detallexItems, dbo.fa_factura_det.vt_por_iva, dbo.fa_factura_det.IdPunto_Cargo, dbo.fa_factura_det.IdPunto_cargo_grupo, dbo.fa_factura_det.IdCod_Impuesto_Iva, 
+                          dbo.fa_factura_det.IdCentroCosto,  dbo.fa_factura_det.IdEmpresa_pf, dbo.fa_factura_det.IdSucursal_pf, 
                          dbo.fa_factura_det.IdProforma, dbo.fa_factura_det.Secuencia_pf, dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_num_lote, dbo.in_Producto.lote_fecha_vcto, 
                          dbo.in_Producto.se_distribuye, dbo.in_ProductoTipo.tp_ManejaInven
 FROM            dbo.in_presentacion RIGHT OUTER JOIN
--- ERP_database/ERP_database/web/Views/vwfa_notaCreDeb_det.sql
+++ ERP_database/ERP_database/web/Views/vwfa_notaCreDeb_det.sql
@@ -3,8 +3,8 @@ AS
 SELECT        dbo.fa_notaCreDeb_det.IdEmpresa, dbo.fa_notaCreDeb_det.IdSucursal, dbo.fa_notaCreDeb_det.IdBodega, dbo.fa_notaCreDeb_det.IdNota, dbo.fa_notaCreDeb_det.Secuencia, dbo.fa_notaCreDeb_det.IdProducto, 
                          dbo.fa_notaCreDeb_det.sc_cantidad, dbo.fa_notaCreDeb_det.sc_Precio, dbo.fa_notaCreDeb_det.sc_descUni, dbo.fa_notaCreDeb_det.sc_PordescUni, dbo.fa_notaCreDeb_det.sc_precioFinal, dbo.fa_notaCreDeb_det.sc_subtotal, 
                          dbo.fa_notaCreDeb_det.sc_iva, dbo.fa_notaCreDeb_det.sc_total, dbo.fa_notaCreDeb_det.sc_costo, dbo.fa_notaCreDeb_det.sc_observacion, dbo.fa_notaCreDeb_det.sc_estado, dbo.fa_notaCreDeb_det.vt_por_iva, 
-                         dbo.fa_notaCreDeb_det.IdPunto_Cargo, dbo.fa_notaCreDeb_det.IdPunto_cargo_grupo, dbo.fa_notaCreDeb_det.IdCod_Impuesto_Iva, dbo.fa_notaCreDeb_det.IdCod_Impuesto_Ice, dbo.fa_notaCreDeb_det.IdCentroCosto, 
-                         dbo.fa_notaCreDeb_det.IdCentroCosto_sub_centro_costo, dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_fecha_vcto, dbo.in_Producto.lote_num_lote, 
+                         dbo.fa_notaCreDeb_det.IdPunto_Cargo, dbo.fa_notaCreDeb_det.IdPunto_cargo_grupo, dbo.fa_notaCreDeb_det.IdCod_Impuesto_Iva, dbo.fa_notaCreDeb_det.IdCentroCosto, 
+                         dbo.in_Producto.pr_descripcion, dbo.in_presentacion.nom_presentacion, dbo.in_Producto.lote_fecha_vcto, dbo.in_Producto.lote_num_lote, 
                          dbo.fa_notaCreDeb_det.sc_cantidad_factura
 FROM            dbo.in_presentacion INNER JOIN
                          dbo.in_Producto ON dbo.in_presentacion.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_presentacion.IdPresentacion = dbo.in_Producto.IdPresentacion RIGHT OUTER JOIN
