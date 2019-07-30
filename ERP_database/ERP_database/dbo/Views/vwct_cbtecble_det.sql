2b855e47332d5faaadbd08b9d730390504946930
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-07-30 06:50:26 -0500

    Update vwct_cbtecble_det.sql

--- ERP_database/ERP_database/dbo/Views/vwct_cbtecble_det.sql
+++ ERP_database/ERP_database/dbo/Views/vwct_cbtecble_det.sql
@@ -1,10 +1,13 @@
 CREATE VIEW dbo.vwct_cbtecble_det
 AS
-SELECT        dbo.ct_cbtecble_det.IdEmpresa, dbo.ct_cbtecble_det.IdTipoCbte, dbo.ct_cbtecble_det.IdCbteCble, dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_cbtecble_det.dc_Valor, 
-                         dbo.ct_cbtecble_det.dc_Observacion, dbo.ct_cbtecble_det.dc_para_conciliar, dbo.ct_cbtecble_det.IdCtaCble + ' - ' + dbo.ct_plancta.pc_Cuenta AS pc_Cuenta, dbo.ct_cbtecble_det.IdPunto_cargo_grupo, 
-                         dbo.ct_cbtecble_det.IdPunto_cargo, dbo.ct_cbtecble_det.IdCentroCosto
-FROM            dbo.ct_cbtecble_det INNER JOIN
-                         dbo.ct_plancta ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_plancta.IdEmpresa AND dbo.ct_cbtecble_det.IdCtaCble = dbo.ct_plancta.IdCtaCble
+SELECT dbo.ct_cbtecble_det.IdEmpresa, dbo.ct_cbtecble_det.IdTipoCbte, dbo.ct_cbtecble_det.IdCbteCble, dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_cbtecble_det.dc_Valor, dbo.ct_cbtecble_det.dc_Observacion, 
+                  dbo.ct_cbtecble_det.dc_para_conciliar, dbo.ct_cbtecble_det.IdCtaCble + ' - ' + dbo.ct_plancta.pc_Cuenta AS pc_Cuenta, dbo.ct_cbtecble_det.IdPunto_cargo_grupo, dbo.ct_cbtecble_det.IdPunto_cargo, dbo.ct_cbtecble_det.IdCentroCosto, 
+                  dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo, dbo.ct_punto_cargo.nom_punto_cargo, dbo.ct_CentroCosto.cc_Descripcion
+FROM     dbo.ct_cbtecble_det INNER JOIN
+                  dbo.ct_plancta ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_plancta.IdEmpresa AND dbo.ct_cbtecble_det.IdCtaCble = dbo.ct_plancta.IdCtaCble LEFT OUTER JOIN
+                  dbo.ct_punto_cargo_grupo ON dbo.ct_cbtecble_det.IdPunto_cargo_grupo = dbo.ct_punto_cargo_grupo.IdPunto_cargo_grupo AND dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo_grupo.IdEmpresa LEFT OUTER JOIN
+                  dbo.ct_punto_cargo ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo.IdEmpresa AND dbo.ct_cbtecble_det.IdPunto_cargo = dbo.ct_punto_cargo.IdPunto_cargo LEFT OUTER JOIN
+                  dbo.ct_CentroCosto ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_CentroCosto.IdEmpresa AND dbo.ct_cbtecble_det.IdCentroCosto = dbo.ct_CentroCosto.IdCentroCosto
 GO
 
 
