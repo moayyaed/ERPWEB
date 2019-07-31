b1e10a14fa49cb5c31a36417a21906d3ac6c8012
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-07-31 14:17:28 -0500

    9

--- ERP_database/ERP_database/dbo/Tables/cp_TipoDocumento.sql
+++ ERP_database/ERP_database/dbo/Tables/cp_TipoDocumento.sql
@@ -6,19 +6,19 @@
     [DeclaraSRI]                      VARCHAR (1)   NOT NULL,
     [CodSRI]                          VARCHAR (5)   NULL,
     [Estado]                          VARCHAR (1)   NOT NULL,
-    [IdUsuario]                       VARCHAR (20)  NOT NULL,
-    [Fecha_Transac]                   DATETIME      NOT NULL,
+    [IdUsuario]                       VARCHAR (20)  NULL,
+    [Fecha_Transac]                   DATETIME      NULL,
     [IdUsuarioUltMod]                 VARCHAR (20)  NULL,
     [Fecha_UltMod]                    DATETIME      NULL,
     [IdUsuarioUltAnu]                 VARCHAR (20)  NULL,
     [Fecha_UltAnu]                    DATETIME      NULL,
-    [nom_pc]                          VARCHAR (50)  NULL,
-    [ip]                              VARCHAR (50)  NULL,
     [GeneraRetencion]                 VARCHAR (1)   NULL,
     [Codigo_Secuenciales_Transaccion] VARCHAR (250) NULL,
     [Sustento_Tributario]             VARCHAR (250) NULL,
     CONSTRAINT [PK_cp_TipoDocumento] PRIMARY KEY CLUSTERED ([CodTipoDocumento] ASC)
 );
+
+
 
 
 GO
