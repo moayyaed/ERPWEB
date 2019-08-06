4bcddd316d9a9f35995bba59848acd86eec7e970
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-08-06 17:09:20 -0500

    ..

--- ERP_database/ERP_database/dbo/Views/vwtb_comprobantes_no_autorizados.sql
+++ ERP_database/ERP_database/dbo/Views/vwtb_comprobantes_no_autorizados.sql
@@ -7,16 +7,14 @@ FROM     dbo.tb_persona AS per INNER JOIN
                   dbo.fa_factura AS doc ON cont.IdEmpresa = doc.IdEmpresa AND cont.IdCliente = doc.IdCliente AND doc.Estado = 'A' AND 
                   doc.aprobada_enviar_sri = 0
 /*where 'RAP'+'-'+'FAC'+'-'+doc.vt_serie1+'-'+doc.vt_serie2+'-'+doc.vt_NumFactura NOT IN (SELEct id_registro from EntidadRegulatoria.fa_elec_registros_generados)*/ UNION
-SELECT doc.IdEmpresa, 'RETEN', doc.IdRetencion, doc.serie1, doc.serie2, doc.NumRetencion Documento, doc.serie1 + '-' + doc.serie2 + '-' + doc.NumRetencion, doc.fecha, per.pe_nombreCompleto, doc.observacion,
-fp.IdSucursal
+SELECT doc.IdEmpresa, 'RETEN', doc.IdRetencion, doc.serie1, doc.serie2, doc.NumRetencion Documento, doc.serie1 + '-' + doc.serie2 + '-' + doc.NumRetencion, doc.fecha, per.pe_nombreCompleto, doc.observacion, fp.IdSucursal
 FROM     dbo.tb_persona AS per INNER JOIN
                   dbo.cp_proveedor AS cont ON per.IdPersona = cont.IdPersona INNER JOIN
                   dbo.cp_orden_giro AS fp ON cont.IdEmpresa = fp.IdEmpresa AND cont.IdProveedor = fp.IdProveedor INNER JOIN
                   dbo.cp_retencion AS doc ON fp.IdEmpresa = doc.IdEmpresa_Ogiro AND fp.IdCbteCble_Ogiro = doc.IdCbteCble_Ogiro AND fp.IdTipoCbte_Ogiro = doc.IdTipoCbte_Ogiro AND doc.Estado = 'A' AND doc.aprobada_enviar_sri = 0 AND 
                   doc.NumRetencion IS NOT NULL
 /* where 'RAP'+'-'+'RET'+'-'+doc.serie1+'-'+doc.serie2+'-'+doc.NumRetencion NOT IN (SELEct id_registro from EntidadRegulatoria.fa_elec_registros_generados)*/ UNION
-SELECT cont.IdEmpresa, 'NTCR', doc.IdNota, doc.Serie1, doc.Serie2, doc.NumNota_Impresa Documento, doc.Serie1 + '-' + doc.Serie2 + '-' + doc.NumNota_Impresa, doc.no_fecha, per.pe_nombreCompleto, doc.sc_observacion,
-doc.IdSucursal
+SELECT cont.IdEmpresa, 'NTCR', doc.IdNota, doc.Serie1, doc.Serie2, doc.NumNota_Impresa Documento, doc.Serie1 + '-' + doc.Serie2 + '-' + doc.NumNota_Impresa, doc.no_fecha, per.pe_nombreCompleto, doc.sc_observacion, doc.IdSucursal
 FROM     dbo.tb_persona AS per INNER JOIN
                   dbo.fa_cliente AS cont ON per.IdPersona = cont.IdPersona INNER JOIN
                   dbo.fa_notaCreDeb AS doc ON cont.IdEmpresa = doc.IdEmpresa AND cont.IdCliente = doc.IdCliente AND doc.Estado = 'A' AND doc.aprobada_enviar_sri = 0 AND 
@@ -26,7 +24,7 @@ SELECT doc.IdEmpresa, 'GUIA', doc.IdGuiaRemision, doc.Serie1, doc.Serie2, doc.Nu
                   doc.gi_Observacion, doc.IdSucursal
 FROM     dbo.tb_persona AS per INNER JOIN
                   dbo.fa_cliente AS cont ON per.IdPersona = cont.IdPersona INNER JOIN
-                  dbo.fa_guia_remision AS doc ON cont.IdEmpresa = doc.IdEmpresa AND cont.IdCliente = doc.IdCliente AND doc.Estado = 'A' AND doc.aprobada_enviar_sri = 0
+                  dbo.fa_guia_remision AS doc ON cont.IdEmpresa = doc.IdEmpresa AND cont.IdCliente = doc.IdCliente AND doc.Estado = 1 AND doc.aprobada_enviar_sri = 0
 						 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwtb_comprobantes_no_autorizados';
@@ -129,14 +127,14 @@ Begin DesignProperties =
       Begin ColumnWidths = 11
          Column = 1440
          Alias = 900
-         Table = 1170
+         Table = 1176
          Output = 720
          Append = 1400
          NewValue = 1170
-         SortType = 1350
-         SortOrder = 1410
+         SortType = 1356
+         SortOrder = 1416
          GroupBy = 1350
-         Filter = 1350
+         Filter = 1356
          Or = 1350
          Or = 1350
          Or = 1350
@@ -144,4 +142,6 @@ Begin DesignProperties =
    End
 End
 ', @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwtb_comprobantes_no_autorizados';
+
+
 
--- ERP_database/ERP_database/web/Views/vwcxc_cobro_para_retencion.sql
+++ ERP_database/ERP_database/web/Views/vwcxc_cobro_para_retencion.sql
@@ -1,44 +1,49 @@
 ﻿CREATE VIEW [web].[vwcxc_cobro_para_retencion]
 AS
-SELECT dbo.fa_factura_resumen.IdEmpresa, dbo.fa_factura_resumen.IdSucursal, dbo.fa_factura_resumen.IdBodega, dbo.fa_factura_resumen.IdCbteVta, dbo.fa_factura.vt_tipoDoc, ISNULL(dbo.fa_factura_resumen.SubtotalConDscto, 0) AS vt_Subtotal, 
-                  ISNULL(dbo.fa_factura_resumen.ValorIVA, 0) AS vt_iva, ISNULL(dbo.fa_factura_resumen.Total, 0) AS vt_total, tb_persona.pe_nombreCompleto Nombres, dbo.fa_factura.vt_fecha, dbo.fa_factura.vt_fech_venc, 
-                  dbo.fa_factura.vt_Observacion, dbo.fa_factura.vt_tipoDoc + '-' + CAST(CAST(dbo.fa_factura.vt_NumFactura AS numeric) AS varchar(20)) AS vt_NumFactura, dbo.tb_sucursal.Su_Descripcion, dbo.fa_factura.IdCliente,
-				  CASE WHEN RET.IdEmpresa IS NULL THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END AS TieneRetencion
+SELECT dbo.fa_factura_resumen.IdEmpresa, dbo.fa_factura_resumen.IdSucursal, dbo.fa_factura_resumen.IdBodega, dbo.fa_factura_resumen.IdCbteVta, dbo.fa_factura.vt_tipoDoc, ISNULL(dbo.fa_factura_resumen.SubtotalConDscto, 0) 
+                  AS vt_Subtotal, ISNULL(dbo.fa_factura_resumen.ValorIVA, 0) AS vt_iva, ISNULL(dbo.fa_factura_resumen.Total, 0) AS vt_total, tb_persona.pe_nombreCompleto Nombres, dbo.fa_factura.vt_fecha, dbo.fa_factura.vt_fech_venc, 
+                  dbo.fa_factura.vt_Observacion, dbo.fa_factura.vt_serie1+'-'+dbo.fa_factura.vt_serie2+'-'+ dbo.fa_factura.vt_NumFactura  AS vt_NumFactura, dbo.tb_sucursal.Su_Descripcion, dbo.fa_factura.IdCliente, 
+                  CASE WHEN RET.IdEmpresa IS NULL THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END AS TieneRetencion
 FROM     dbo.fa_factura INNER JOIN
                   dbo.fa_factura_resumen ON dbo.fa_factura.IdEmpresa = dbo.fa_factura_resumen.IdEmpresa AND dbo.fa_factura.IdSucursal = dbo.fa_factura_resumen.IdSucursal AND dbo.fa_factura.IdBodega = dbo.fa_factura_resumen.IdBodega AND 
                   dbo.fa_factura.IdCbteVta = dbo.fa_factura_resumen.IdCbteVta INNER JOIN
                   dbo.tb_sucursal ON dbo.fa_factura_resumen.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.fa_factura_resumen.IdSucursal = dbo.tb_sucursal.IdSucursal INNER JOIN
                   fa_cliente ON fa_factura.IdEmpresa = fa_cliente.IdEmpresa AND fa_factura.IdCliente = fa_cliente.IdCliente INNER JOIN
                   tb_persona ON fa_cliente.IdPersona = tb_persona.IdPersona LEFT JOIN
-				  (
-						SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
-						FROM     dbo.cxc_cobro INNER JOIN
-						dbo.cxc_cobro_det ON dbo.cxc_cobro.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND dbo.cxc_cobro.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
-						dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo
-						WHERE  (dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = 'RET') AND (dbo.cxc_cobro_det.estado = 'A') AND (dbo.cxc_cobro.cr_estado = N'A')
-						GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
-				  ) AS RET ON fa_factura.IdEmpresa = RET.IdEmpresa AND fa_factura.IdSucursal = RET.IdSucursal AND fa_factura.IdBodega = RET.IdBodega_Cbte AND fa_factura.IdCbteVta = RET.IdCbte_vta_nota AND fa_factura.vt_tipoDoc = RET.dc_TipoDocumento
+                      (SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
+                       FROM      dbo.cxc_cobro INNER JOIN
+                                         dbo.cxc_cobro_det ON dbo.cxc_cobro.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND dbo.cxc_cobro.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
+                                         dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo
+                       WHERE   (dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = 'RET') AND (dbo.cxc_cobro_det.estado = 'A') AND (dbo.cxc_cobro.cr_estado = N'A')
+                       GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro) 
+                  AS RET ON fa_factura.IdEmpresa = RET.IdEmpresa AND fa_factura.IdSucursal = RET.IdSucursal AND fa_factura.IdBodega = RET.IdBodega_Cbte AND fa_factura.IdCbteVta = RET.IdCbte_vta_nota AND 
+                  fa_factura.vt_tipoDoc = RET.dc_TipoDocumento
 WHERE  (dbo.fa_factura.Estado = 'A')
 UNION ALL
 SELECT dbo.fa_notaCreDeb_det.IdEmpresa, dbo.fa_notaCreDeb_det.IdSucursal, dbo.fa_notaCreDeb_det.IdBodega, dbo.fa_notaCreDeb_det.IdNota, dbo.fa_notaCreDeb.CodDocumentoTipo, ISNULL(SUM(dbo.fa_notaCreDeb_det.sc_subtotal), 0) 
                   AS sc_subtotal, ISNULL(SUM(dbo.fa_notaCreDeb_det.sc_iva), 0) AS sc_iva, ISNULL(SUM(dbo.fa_notaCreDeb_det.sc_total), 0) AS sc_total, dbo.tb_persona.pe_nombreCompleto, dbo.fa_notaCreDeb.no_fecha, 
-                  dbo.fa_notaCreDeb.no_fecha_venc, dbo.fa_notaCreDeb.sc_observacion, dbo.fa_notaCreDeb.CodDocumentoTipo + '-' + CASE WHEN fa_notaCreDeb.NumNota_Impresa IS NULL 
-                  THEN fa_notaCreDeb.CodNota ELSE CAST(CAST(fa_notaCreDeb.NumNota_Impresa AS numeric) AS varchar(20)) END AS vt_NumFactura, dbo.tb_sucursal.Su_Descripcion, dbo.fa_cliente.IdCliente,
-				  CASE WHEN RET.IdEmpresa IS NULL THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END AS TieneRetencion
+                  dbo.fa_notaCreDeb.no_fecha_venc, dbo.fa_notaCreDeb.sc_observacion, 
+				  CASE WHEN fa_notaCreDeb.NaturalezaNota = 'SRI' THEN
+				  fa_notaCreDeb.Serie1+'-'+fa_notaCreDeb.Serie2+'-'+ fa_notaCreDeb.NumNota_Impresa 
+				  ELSE ISNULL(fa_notaCreDeb.CodNota,CAST(fa_notaCreDeb.IdNota AS varchar(20))) END AS vt_NumFactura, 
+				  
+				  dbo.tb_sucursal.Su_Descripcion, dbo.fa_cliente.IdCliente, CASE WHEN RET.IdEmpresa IS NULL 
+                  THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END AS TieneRetencion
 FROM     dbo.fa_notaCreDeb INNER JOIN
                   dbo.fa_notaCreDeb_det ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_notaCreDeb_det.IdEmpresa AND dbo.fa_notaCreDeb.IdSucursal = dbo.fa_notaCreDeb_det.IdSucursal AND 
                   dbo.fa_notaCreDeb.IdBodega = dbo.fa_notaCreDeb_det.IdBodega AND dbo.fa_notaCreDeb.IdNota = dbo.fa_notaCreDeb_det.IdNota INNER JOIN
                   dbo.fa_cliente ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_cliente.IdEmpresa AND dbo.fa_notaCreDeb.IdCliente = dbo.fa_cliente.IdCliente INNER JOIN
                   dbo.tb_persona ON dbo.fa_cliente.IdPersona = dbo.tb_persona.IdPersona INNER JOIN
                   dbo.tb_sucursal ON dbo.fa_notaCreDeb_det.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.fa_notaCreDeb_det.IdSucursal = dbo.tb_sucursal.IdSucursal LEFT JOIN
-				  (
-						SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
-						FROM     dbo.cxc_cobro INNER JOIN
-						dbo.cxc_cobro_det ON dbo.cxc_cobro.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND dbo.cxc_cobro.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
-						dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo
-						WHERE  (dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = 'RET') AND (dbo.cxc_cobro_det.estado = 'A') AND (dbo.cxc_cobro.cr_estado = N'A')
-						GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
-				  ) AS RET ON fa_notaCreDeb.IdEmpresa = RET.IdEmpresa AND fa_notaCreDeb.IdSucursal = RET.IdSucursal AND fa_notaCreDeb.IdBodega = RET.IdBodega_Cbte AND fa_notaCreDeb.IdNota = RET.IdCbte_vta_nota AND fa_notaCreDeb.CodDocumentoTipo = RET.dc_TipoDocumento
+                      (SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro
+                       FROM      dbo.cxc_cobro INNER JOIN
+                                         dbo.cxc_cobro_det ON dbo.cxc_cobro.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND dbo.cxc_cobro.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
+                                         dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo
+                       WHERE   (dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = 'RET') AND (dbo.cxc_cobro_det.estado = 'A') AND (dbo.cxc_cobro.cr_estado = N'A')
+                       GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro) 
+                  AS RET ON fa_notaCreDeb.IdEmpresa = RET.IdEmpresa AND fa_notaCreDeb.IdSucursal = RET.IdSucursal AND fa_notaCreDeb.IdBodega = RET.IdBodega_Cbte AND fa_notaCreDeb.IdNota = RET.IdCbte_vta_nota AND 
+                  fa_notaCreDeb.CodDocumentoTipo = RET.dc_TipoDocumento
 WHERE  (dbo.fa_notaCreDeb.CreDeb = 'D')
 GROUP BY dbo.fa_notaCreDeb_det.IdEmpresa, dbo.fa_notaCreDeb_det.IdSucursal, dbo.fa_notaCreDeb_det.IdBodega, dbo.fa_notaCreDeb_det.IdNota, dbo.fa_notaCreDeb.CodDocumentoTipo, dbo.tb_persona.pe_nombreCompleto, 
-                  dbo.fa_notaCreDeb.no_fecha, dbo.fa_notaCreDeb.no_fecha_venc, dbo.fa_notaCreDeb.sc_observacion, dbo.fa_notaCreDeb.CodNota, dbo.fa_notaCreDeb.NumNota_Impresa, dbo.tb_sucursal.Su_Descripcion, dbo.fa_cliente.IdCliente,RET.IdEmpresa
+                  dbo.fa_notaCreDeb.no_fecha, dbo.fa_notaCreDeb.no_fecha_venc, dbo.fa_notaCreDeb.sc_observacion, dbo.fa_notaCreDeb.CodNota, dbo.fa_notaCreDeb.Serie1,dbo.fa_notaCreDeb.Serie2,dbo.fa_notaCreDeb.IdNota,dbo.fa_notaCreDeb.NaturalezaNota, dbo.fa_notaCreDeb.NumNota_Impresa, dbo.tb_sucursal.Su_Descripcion, dbo.fa_cliente.IdCliente, 
+                  RET.IdEmpresa
