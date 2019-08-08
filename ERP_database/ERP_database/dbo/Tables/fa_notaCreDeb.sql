33d962594c31ccf7c9461981e948f88c0e168699
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-08-08 11:12:05 -0500

    a

--- ERP_database/ERP_database/dbo/Tables/fa_notaCreDeb.sql
+++ ERP_database/ERP_database/dbo/Tables/fa_notaCreDeb.sql
@@ -6,7 +6,7 @@
     [dev_IdEmpresa]       INT           NULL,
     [dev_IdDev_Inven]     NUMERIC (18)  NULL,
     [CodNota]             VARCHAR (50)  NULL,
-    [CreDeb]              CHAR (3)      NOT NULL,
+    [CreDeb]              VARCHAR (1)   NOT NULL,
     [CodDocumentoTipo]    VARCHAR (20)  NOT NULL,
     [Serie1]              VARCHAR (3)   NULL,
     [Serie2]              VARCHAR (3)   NULL,
@@ -30,15 +30,19 @@
     [IdPuntoVta]          INT           NOT NULL,
     [aprobada_enviar_sri] BIT           NOT NULL,
     [Generado]            BIT           NULL,
+    [IdVendedor]          INT           NULL,
     CONSTRAINT [PK_fa_notaCreDeb] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdSucursal] ASC, [IdBodega] ASC, [IdNota] ASC),
     CONSTRAINT [FK_fa_notaCreDeb_ct_plancta] FOREIGN KEY ([IdEmpresa], [IdCtaCble_TipoNota]) REFERENCES [dbo].[ct_plancta] ([IdEmpresa], [IdCtaCble]),
     CONSTRAINT [FK_fa_notaCreDeb_fa_cliente] FOREIGN KEY ([IdEmpresa], [IdCliente]) REFERENCES [dbo].[fa_cliente] ([IdEmpresa], [IdCliente]),
     CONSTRAINT [FK_fa_notaCreDeb_fa_PuntoVta] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdPuntoVta]) REFERENCES [dbo].[fa_PuntoVta] ([IdEmpresa], [IdSucursal], [IdPuntoVta]),
     CONSTRAINT [FK_fa_notaCreDeb_fa_TipoNota1] FOREIGN KEY ([IdEmpresa], [IdTipoNota]) REFERENCES [dbo].[fa_TipoNota] ([IdEmpresa], [IdTipoNota]),
+    CONSTRAINT [FK_fa_notaCreDeb_fa_Vendedor] FOREIGN KEY ([IdEmpresa], [IdVendedor]) REFERENCES [dbo].[fa_Vendedor] ([IdEmpresa], [IdVendedor]),
     CONSTRAINT [FK_fa_notaCreDeb_in_devolucion_inven] FOREIGN KEY ([dev_IdEmpresa], [dev_IdDev_Inven]) REFERENCES [dbo].[in_devolucion_inven] ([IdEmpresa], [IdDev_Inven]),
     CONSTRAINT [FK_fa_notaCreDeb_tb_bodega] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdBodega]) REFERENCES [dbo].[tb_bodega] ([IdEmpresa], [IdSucursal], [IdBodega]),
     CONSTRAINT [FK_fa_notaCreDeb_tb_sis_Documento_Tipo_Talonario] FOREIGN KEY ([IdEmpresa], [CodDocumentoTipo], [Serie2], [Serie1], [NumNota_Impresa]) REFERENCES [dbo].[tb_sis_Documento_Tipo_Talonario] ([IdEmpresa], [CodDocumentoTipo], [PuntoEmision], [Establecimiento], [NumDocumento])
 );
+
+
 
 
 
