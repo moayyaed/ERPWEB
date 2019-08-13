6e0f216c2730bb50df8297e1207e97dc474f42f2
WendyCherres <wendycherres@gmail.com>
2019-08-13 14:59:14 -0500

    ..

---
+++ ERP/Core.Erp.Data/Compras/com_ordencompra_local_resumen_Data.cs
@@ -0,0 +1,48 @@
+﻿using Core.Erp.Data.Compras.Base;
+using Core.Erp.Info.Compras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Data.Compras
+{
+    public class com_ordencompra_local_resumen_Data
+    {
+        public com_ordencompra_local_resumen_Info get_info(int IdEmpresa, int IdSucursal, decimal IdOrdenCompra)
+        {
+            try
+            {
+                com_ordencompra_local_resumen_Info info = new com_ordencompra_local_resumen_Info();
+                using (Entities_compras Context = new Entities_compras())
+                {
+                    com_ordencompra_local_resumen Entity = Context.com_ordencompra_local_resumen.Where(q => q.IdEmpresa == IdEmpresa && q.IdSucursal == IdSucursal && q.IdOrdenCompra == IdOrdenCompra).FirstOrDefault();
+                    if (Entity == null) return null;
+
+                    info = new com_ordencompra_local_resumen_Info
+                    {
+                        IdEmpresa = Entity.IdEmpresa,
+                        IdSucursal = Entity.IdSucursal,
+                        IdOrdenCompra = Entity.IdOrdenCompra,
+                        SubtotalIVASinDscto = Entity.SubtotalIVASinDscto,
+                        SubtotalSinIVASinDscto = Entity.SubtotalSinIVASinDscto,
+                        SubtotalSinDscto = Entity.SubtotalSinDscto,
+                        Descuento = Entity.Descuento,
+                        SubtotalIVAConDscto = Entity.SubtotalIVAConDscto,
+                        SubtotalSinIVAConDscto = Entity.SubtotalSinIVAConDscto,
+                        SubtotalConDscto = Entity.SubtotalConDscto,
+                        ValorIVA = Entity.ValorIVA,
+                        Total = Entity.Total
+                    };
+                }
+                return info;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ERP/Core.Erp.Info/Compras/com_ordencompra_local_resumen_Info.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Info.Compras
+{
+    public class com_ordencompra_local_resumen_Info
+    {
+        public int IdEmpresa { get; set; }
+        public int IdSucursal { get; set; }
+        public decimal IdOrdenCompra { get; set; }
+        public decimal SubtotalIVASinDscto { get; set; }
+        public decimal SubtotalSinIVASinDscto { get; set; }
+        public decimal SubtotalSinDscto { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal SubtotalIVAConDscto { get; set; }
+        public decimal SubtotalSinIVAConDscto { get; set; }
+        public decimal SubtotalConDscto { get; set; }
+        public decimal ValorIVA { get; set; }
+        public decimal Total { get; set; }
+    }
+}
