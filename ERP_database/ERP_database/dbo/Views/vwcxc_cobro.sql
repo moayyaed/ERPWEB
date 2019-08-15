a1ee5874271af21c78057be87ace6f2590ca9e5c
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-08-15 14:10:29 -0500

    .

--- ERP_database/ERP_database/dbo/Views/vwcxc_cobro.sql
+++ ERP_database/ERP_database/dbo/Views/vwcxc_cobro.sql
@@ -3,7 +3,7 @@ CREATE VIEW [dbo].[vwcxc_cobro]
 AS
 SELECT     FC.IdPersona, C.IdEmpresa, C.IdSucursal, C.IdCobro, C.IdCobro_tipo, C.IdCliente, C.cr_TotalCobro, C.cr_fecha, C.cr_fechaCobro, C.cr_fechaDocu, C.cr_observacion, 
                       C.cr_Banco, C.cr_cuenta, C.cr_Tarjeta, C.cr_NumDocumento, C.cr_estado, C.cr_recibo, C.Fecha_Transac, C.IdUsuario, C.IdUsuarioUltMod, C.Fecha_UltMod, 
-                      C.IdUsuarioUltAnu, C.Fecha_UltAnu, C.nom_pc, C.ip, S.Su_Descripcion AS nSucursal, P.pe_nombreCompleto AS nCliente, '' AS cr_NumCheque, 
+                      C.IdUsuarioUltAnu, C.Fecha_UltAnu, '' nom_pc, '' ip, S.Su_Descripcion AS nSucursal, P.pe_nombreCompleto AS nCliente, '' AS cr_NumCheque, 
                       1 AS IdVendedorCliente, C.cr_Codigo, C.IdTipoNotaCredito, C.cr_propietarioCta, C.IdBanco, C.IdCaja, C.cr_es_anticipo
 FROM         dbo.tb_persona AS P INNER JOIN
                       dbo.tb_sucursal AS S INNER JOIN
