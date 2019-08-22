862f24c119ce88c3739095f106c97da85948fd67
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-08-22 15:10:19 -0500

    s

--- ERP_database/ERP_database/web/Stored Procedures/SPCXC_009.sql
+++ ERP_database/ERP_database/web/Stored Procedures/SPCXC_009.sql
@@ -1,4 +1,4 @@
-﻿
+﻿--exec [web].[SPCXC_009] 1,70,'22/08/2019'
 CREATE PROCEDURE [web].[SPCXC_009]
 (
 @IdEmpresa int,
@@ -36,13 +36,13 @@ and round(fa_factura_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) >  0
 UNION ALL
 SELECT fa_notaCreDeb.IdEmpresa, fa_notaCreDeb.IdSucursal, fa_notaCreDeb.IdBodega, fa_notaCreDeb.IdNota,fa_notaCreDeb.CodDocumentoTipo, 
 isnull(fa_notaCreDeb.Serie1 + '-' + fa_notaCreDeb.Serie2 + '-' + fa_notaCreDeb.NumNota_Impresa, fa_notaCreDeb.CodNota)AS NumDocumento, fa_notaCreDeb.no_fecha, fa_notaCreDeb.no_fecha_venc, 
-                  tb_sucursal.Su_Descripcion, fa_notaCreDeb.IdCliente, tb_persona.pe_nombreCompleto, tb_persona.pe_cedulaRuc, 0, '' Ve_Vendedor, fa_notaCreDeb_resumen.Total, CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) < 0 THEN 0 ELSE DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) END DiasVencido,
+                  tb_sucursal.Su_Descripcion, fa_notaCreDeb.IdCliente, tb_persona.pe_nombreCompleto, tb_persona.pe_cedulaRuc, 0, '' Ve_Vendedor, fa_notaCreDeb_resumen.Total, CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) < 0 THEN 0 ELSE DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) END DiasVencido,
 				  isnull(COBRO.dc_ValorPago,0) TotalCobrado, round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) as Saldo, fa_notaCreDeb.sc_observacion,
-				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) < 0  THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS X_VENCER,
-				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) BETWEEN 1 AND 30  THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO30, 
-				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) BETWEEN 31 AND 60 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO60,
-				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) BETWEEN 61 AND 90 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO90,
-				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha,@FechaCorte) >= 91 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO90MAS
+				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) < 0  THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS X_VENCER,
+				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) BETWEEN 1 AND 30  THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO30, 
+				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) BETWEEN 31 AND 60 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO60,
+				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) BETWEEN 61 AND 90 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO90,
+				  CASE WHEN DATEDIFF(DAY,fa_notaCreDeb.no_fecha_venc,@FechaCorte) >= 91 THEN round(fa_notaCreDeb_resumen.Total - isnull(COBRO.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO90MAS
 FROM     fa_notaCreDeb INNER JOIN
                   fa_cliente ON fa_notaCreDeb.IdEmpresa = fa_cliente.IdEmpresa AND fa_notaCreDeb.IdCliente = fa_cliente.IdCliente INNER JOIN
                   tb_persona ON fa_cliente.IdPersona = tb_persona.IdPersona INNER JOIN
