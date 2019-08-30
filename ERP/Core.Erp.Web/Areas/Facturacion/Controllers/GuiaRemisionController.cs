121502204dc918a361981bf1b5f822a302849969
AROCA LAPTOP <39768774+arocajorge@users.noreply.github.com>
2019-08-30 13:57:21 -0500

    s

--- ERP/Core.Erp.Web/Areas/Facturacion/Controllers/GuiaRemisionController.cs
+++ ERP/Core.Erp.Web/Areas/Facturacion/Controllers/GuiaRemisionController.cs
@@ -252,17 +252,21 @@ namespace Core.Erp.Web.Areas.Facturacion.Controllers
                 {
                     cargar_combos(model);
                     ViewBag.mensaje = mensaje;
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
                 if (!validar(model, ref mensaje))
                 {
                     cargar_combos(model);
                     ViewBag.mensaje = mensaje;
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
                 if (!bus_guia.guardarDB(model))
                 {
+                    ViewBag.mensaje = "Ha ocurrido un problema, comuniquese con sistemas";
                     cargar_combos(model);
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
 
@@ -328,19 +332,23 @@ namespace Core.Erp.Web.Areas.Facturacion.Controllers
                 string mensaje = bus_guia.validar(model);
                 if (!validar(model, ref mensaje))
                 {
+                    ViewBag.mensaje = "Ha ocurrido un problema, comuniquese con sistemas";
                     cargar_combos(model);
-                    ViewBag.mensaje = mensaje;
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
                 if (mensaje != "")
                 {
+                    ViewBag.mensaje = "Ha ocurrido un problema, comuniquese con sistemas";
                     cargar_combos(model);
-                    ViewBag.mensaje = mensaje;
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
                 if (!bus_guia.modificarDB(model))
                 {
+                    ViewBag.mensaje = "Ha ocurrido un problema, comuniquese con sistemas";
                     cargar_combos(model);
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
 
@@ -392,7 +400,9 @@ namespace Core.Erp.Web.Areas.Facturacion.Controllers
             {
                 if (!bus_guia.anularDB(model))
                 {
+                    ViewBag.mensaje = "Ha ocurrido un problema, comuniquese con sistemas";
                     cargar_combos(model);
+                    SessionFixed.IdTransaccionSessionActual = model.IdTransaccionSession.ToString();
                     return View(model);
                 }
                 Session["fa_guia_remision_det_Info"] = null;
