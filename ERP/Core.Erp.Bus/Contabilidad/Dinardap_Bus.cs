f10648b846ede9c9396eaa9f03ea1c90ac4f67de
WendyCherres <wendycherres@gmail.com>
2019-09-05 11:52:12 -0500

    ..

---
+++ ERP/Core.Erp.Bus/Contabilidad/Dinardap_Bus.cs
@@ -0,0 +1,29 @@
+﻿using Core.Erp.Data.Contabilidad;
+using Core.Erp.Info.Contabilidad;
+using Core.Erp.Info.Reportes.CuentasPorCobrar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Erp.Bus.Contabilidad
+{
+    public class Dinardap_Bus
+    {
+        Dinardap_Data oData = new Dinardap_Data();
+
+        public List<CXC_010_Info> get_info(int IdEmpresa, int IdPeriodo, int IdSucursal)
+        {
+            try
+            {
+                return oData.get_info(IdEmpresa, IdPeriodo, IdSucursal);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
