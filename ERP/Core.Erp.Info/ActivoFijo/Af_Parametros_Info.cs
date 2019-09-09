c5e9a623b3163e150ec6cd9ea4d8f1f35ef6caff
WendyCherres <wendycherres@gmail.com>
2019-09-09 17:47:21 -0500

    ...

--- ERP/Core.Erp.Info/ActivoFijo/Af_Parametros_Info.cs
+++ ERP/Core.Erp.Info/ActivoFijo/Af_Parametros_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,17 @@ namespace Core.Erp.Info.ActivoFijo
     public class Af_Parametros_Info
     {
         public int IdEmpresa { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "El campo tipo de comprobante es obligatorio")]
         public int IdTipoCbte { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "El campo tipo de comprobante para mejora es obligatorio")]
         public int IdTipoCbteMejora { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "El campo tipo de comprobante para baja es obligatorio")]
         public int IdTipoCbteBaja { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "El campo tipo de comprobante para venta es obligatorio")]
         public int IdTipoCbteVenta { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "El campo tipo de comprobante para retiro es obligatorio")]
         public int IdTipoCbteRetiro { get; set; }
+        [Required(ErrorMessage = "El campo dias de transacción a futuro es obligatorio")]
         public int DiasTransaccionesAFuturo { get; set; }
         public bool ContabilizaDepreciacionPorActivo { get; set; }
     }
