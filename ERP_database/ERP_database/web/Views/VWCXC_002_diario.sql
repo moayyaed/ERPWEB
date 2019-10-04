c3d300b674e517354b0ad0ab4057c1207b5636e0
Jorge Aroca <jaroca@fixed-sa.com>
2019-10-04 12:05:40 -0500

    .

--- ERP_database/ERP_database/web/Views/VWCXC_002_diario.sql
+++ ERP_database/ERP_database/web/Views/VWCXC_002_diario.sql
@@ -1,22 +1,24 @@
-﻿CREATE VIEW web.VWCXC_002_diario
-AS
-SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, 1 AS secuencial, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_det.IdBodega_Cbte, 
-                  dbo.cxc_cobro_det.IdCbte_vta_nota, dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa AS IdEmpresa_ct, dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte AS IdTipoCbte_ct, 
-                  dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble AS IdCbteCble_ct, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, 
-                  CASE WHEN ct_cbtecble_det.dc_Valor > 0 THEN ct_cbtecble_det.dc_Valor ELSE 0 END AS dc_Valor_Debe, CASE WHEN ct_cbtecble_det.dc_Valor < 0 THEN ABS(ct_cbtecble_det.dc_Valor) 
-                  ELSE 0 END AS dc_Valor_Haber
-FROM     dbo.cxc_cobro AS cxc_cobro_1 INNER JOIN
-                  dbo.cxc_cobro_det ON cxc_cobro_1.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND cxc_cobro_1.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND cxc_cobro_1.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
-                  dbo.cxc_cobro_x_ct_cbtecble ON cxc_cobro_1.IdEmpresa = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdEmpresa AND cxc_cobro_1.IdSucursal = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdSucursal AND 
-                  cxc_cobro_1.IdCobro = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdCobro AND cxc_cobro_1.cr_estado <> 'I' INNER JOIN
-                  dbo.ct_plancta INNER JOIN
-                  dbo.ct_cbtecble_det ON dbo.ct_plancta.IdEmpresa = dbo.ct_cbtecble_det.IdEmpresa AND dbo.ct_plancta.IdCtaCble = dbo.ct_cbtecble_det.IdCtaCble ON 
-                  dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa = dbo.ct_cbtecble_det.IdEmpresa AND dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte = dbo.ct_cbtecble_det.IdTipoCbte AND 
-                  dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble = dbo.ct_cbtecble_det.IdCbteCble
-WHERE  (cxc_cobro_1.IdCobro_tipo IS NULL)
+﻿CREATE view [web].[VWCXC_002_diario]
+as
+SELECT        dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, 1 AS secuencial, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, 
+                         dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa AS IdEmpresa_ct, dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte AS IdTipoCbte_ct, dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble AS IdCbteCble_ct, dbo.ct_cbtecble_det.IdCtaCble, 
+                         dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, CASE WHEN ct_cbtecble_det.dc_Valor > 0 THEN ct_cbtecble_det.dc_Valor ELSE 0 END AS dc_Valor_Debe, 
+                         CASE WHEN ct_cbtecble_det.dc_Valor < 0 THEN ABS(ct_cbtecble_det.dc_Valor) ELSE 0 END AS dc_Valor_Haber, dbo.ct_punto_cargo.nom_punto_cargo, dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo, 
+                         dbo.ct_cbtecble_det.IdPunto_cargo_grupo, dbo.ct_cbtecble_det.IdPunto_cargo
+FROM            dbo.cxc_cobro AS cxc_cobro_1 INNER JOIN
+                         dbo.cxc_cobro_det ON cxc_cobro_1.IdEmpresa = dbo.cxc_cobro_det.IdEmpresa AND cxc_cobro_1.IdSucursal = dbo.cxc_cobro_det.IdSucursal AND cxc_cobro_1.IdCobro = dbo.cxc_cobro_det.IdCobro INNER JOIN
+                         dbo.cxc_cobro_x_ct_cbtecble ON cxc_cobro_1.IdEmpresa = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdEmpresa AND cxc_cobro_1.IdSucursal = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdSucursal AND 
+                         cxc_cobro_1.IdCobro = dbo.cxc_cobro_x_ct_cbtecble.cbr_IdCobro AND cxc_cobro_1.cr_estado <> 'I' INNER JOIN
+                         dbo.ct_plancta INNER JOIN
+                         dbo.ct_cbtecble_det ON dbo.ct_plancta.IdEmpresa = dbo.ct_cbtecble_det.IdEmpresa AND dbo.ct_plancta.IdCtaCble = dbo.ct_cbtecble_det.IdCtaCble ON 
+                         dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa = dbo.ct_cbtecble_det.IdEmpresa AND dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte = dbo.ct_cbtecble_det.IdTipoCbte AND 
+                         dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble = dbo.ct_cbtecble_det.IdCbteCble LEFT OUTER JOIN
+                         dbo.ct_punto_cargo_grupo ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo_grupo.IdEmpresa AND dbo.ct_cbtecble_det.IdPunto_cargo_grupo = dbo.ct_punto_cargo_grupo.IdPunto_cargo_grupo LEFT OUTER JOIN
+                         dbo.ct_punto_cargo ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo.IdEmpresa AND dbo.ct_cbtecble_det.IdPunto_cargo = dbo.ct_punto_cargo.IdPunto_cargo
+WHERE        (cxc_cobro_1.IdCobro_tipo IS NULL)
 GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_det.dc_TipoDocumento, dbo.cxc_cobro_det.IdBodega_Cbte, dbo.cxc_cobro_det.IdCbte_vta_nota, 
-                  dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa, dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte, dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, 
-                  dbo.ct_cbtecble_det.dc_Valor
+                         dbo.cxc_cobro_x_ct_cbtecble.ct_IdEmpresa, dbo.cxc_cobro_x_ct_cbtecble.ct_IdTipoCbte, dbo.cxc_cobro_x_ct_cbtecble.ct_IdCbteCble, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, 
+                         dbo.ct_punto_cargo.nom_punto_cargo, dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo, dbo.ct_cbtecble_det.IdPunto_cargo_grupo, dbo.ct_cbtecble_det.IdPunto_cargo
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 2, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'VWCXC_002_diario';
