0ba9f6faf5901a1ff7e24e78d512242819bb0936
arocajorge <arocajorge92@gmail.com>
2019-10-25 09:11:41 -0500

    8

---
+++ ERP_database/ERP_database/web/Stored Procedures/SPCONTA_012.sql
@@ -0,0 +1,43 @@
+﻿--exec [web].[SPCONTA_012] 1,201910
+CREATE procedure [web].[SPCONTA_012]
+(
+@IdEmpresa int,
+@IdPeriodo int
+)AS
+BEGIN
+--set @IdPeriodo = 201608
+declare @Fecha_ini datetime
+declare @Fecha_fin datetime
+
+select @Fecha_ini = pe_FechaIni, @Fecha_fin = pe_FechaFin from ct_periodo where @IdPeriodo = ct_periodo.IdPeriodo
+
+SELECT        ct_cbtecble.IdEmpresa, ct_grupo_x_Tipo_Gasto.nivel, 
+						 iif(ct_grupo_x_Tipo_Gasto_1.nivel = 1, ct_grupo_x_Tipo_Gasto.orden, ct_grupo_x_Tipo_Gasto_1.orden) as orden, ct_grupo_x_Tipo_Gasto.IdTipo_Gasto, 
+                         iif(ct_grupo_x_Tipo_Gasto_1.nivel = 1, ct_grupo_x_Tipo_Gasto_1.orden,ct_grupo_x_Tipo_Gasto_2.orden) as orden_tipo_gasto,ct_grupo_x_Tipo_Gasto_1.nom_tipo_Gasto, 
+						 iif(ct_grupo_x_Tipo_Gasto_1.nivel = 1, ct_grupo_x_Tipo_Gasto_1.nom_tipo_Gasto,ct_grupo_x_Tipo_Gasto_2.nom_tipo_Gasto) AS nom_tipo_Gasto_padre, 
+						 ISNULL(SUM(ct_cbtecble_det.dc_Valor), 0) 
+                         AS dc_Valor, CAST(ct_grupo_x_Tipo_Gasto.IdTipo_Gasto AS varchar) AS IdCta, ct_grupo_x_Tipo_Gasto.nom_tipo_Gasto AS nom_cuenta,''nom_grupo_CC
+FROM            ct_cbtecble INNER JOIN
+                         ct_cbtecble_det ON ct_cbtecble.IdEmpresa = ct_cbtecble_det.IdEmpresa AND ct_cbtecble.IdTipoCbte = ct_cbtecble_det.IdTipoCbte AND 
+                         ct_cbtecble.IdCbteCble = ct_cbtecble_det.IdCbteCble INNER JOIN
+                         ct_cbtecble_tipo ON ct_cbtecble.IdEmpresa = ct_cbtecble_tipo.IdEmpresa AND ct_cbtecble.IdTipoCbte = ct_cbtecble_tipo.IdTipoCbte INNER JOIN
+                         ct_plancta ON ct_cbtecble_det.IdEmpresa = ct_plancta.IdEmpresa AND ct_cbtecble_det.IdCtaCble = ct_plancta.IdCtaCble inner JOIN
+                         ct_grupo_x_Tipo_Gasto AS ct_grupo_x_Tipo_Gasto_1 INNER JOIN
+                         ct_grupo_x_Tipo_Gasto ON ct_grupo_x_Tipo_Gasto_1.IdEmpresa = ct_grupo_x_Tipo_Gasto.IdEmpresa AND 
+                         ct_grupo_x_Tipo_Gasto_1.IdTipo_Gasto = ct_grupo_x_Tipo_Gasto.IdTipo_Gasto_Padre LEFT OUTER JOIN
+                         ct_grupo_x_Tipo_Gasto AS ct_grupo_x_Tipo_Gasto_2 ON ct_grupo_x_Tipo_Gasto_1.IdEmpresa = ct_grupo_x_Tipo_Gasto_2.IdEmpresa AND 
+                         ct_grupo_x_Tipo_Gasto_1.IdTipo_Gasto_Padre = ct_grupo_x_Tipo_Gasto_2.IdTipo_Gasto ON ct_plancta.IdEmpresa = ct_grupo_x_Tipo_Gasto.IdEmpresa AND 
+                         ct_plancta.IdTipo_Gasto = ct_grupo_x_Tipo_Gasto.IdTipo_Gasto
+where ct_cbtecble.cb_Fecha between @Fecha_ini and @Fecha_fin and ct_cbtecble.IdEmpresa = @IdEmpresa
+and not exists(
+select * from ct_cbtecble_Reversado R 
+where R.IdEmpresa = ct_cbtecble.IdEmpresa and r.IdTipoCbte = ct_cbtecble.IdTipoCbte and r.IdCbteCble = ct_cbtecble.IdCbteCble
+) and not exists(
+select * from ct_cbtecble_Reversado R 
+where R.IdEmpresa_Anu = ct_cbtecble.IdEmpresa and r.IdTipoCbte_Anu = ct_cbtecble.IdTipoCbte and r.IdCbteCble_Anu = ct_cbtecble.IdCbteCble
+)
+GROUP BY ct_cbtecble.IdEmpresa, ct_grupo_x_Tipo_Gasto.nivel, ct_grupo_x_Tipo_Gasto.orden, ct_grupo_x_Tipo_Gasto.IdTipo_Gasto, ct_grupo_x_Tipo_Gasto_1.orden,ct_grupo_x_Tipo_Gasto_2.orden,
+                         ct_grupo_x_Tipo_Gasto.nom_tipo_Gasto,ct_grupo_x_Tipo_Gasto_1.nivel, ct_grupo_x_Tipo_Gasto_1.nom_tipo_Gasto,ct_grupo_x_Tipo_Gasto_2.nom_tipo_Gasto
+
+
+END
--- ERP_database/ERP_database/web/Views/vwfa_notaCreDeb.sql
+++ ERP_database/ERP_database/web/Views/vwfa_notaCreDeb.sql
@@ -1,24 +1,18 @@
-﻿CREATE view  WEB.vwfa_notaCreDeb 
-as
+﻿CREATE VIEW [web].[vwfa_notaCreDeb]
+AS
 SELECT dbo.fa_notaCreDeb.IdEmpresa, dbo.fa_notaCreDeb.IdSucursal, dbo.fa_notaCreDeb.IdBodega, dbo.fa_notaCreDeb.IdNota, dbo.fa_notaCreDeb.CreDeb, 
                   dbo.fa_notaCreDeb.Serie1 + '-' + dbo.fa_notaCreDeb.Serie2 + '-' + dbo.fa_notaCreDeb.NumNota_Impresa AS NumNota_Impresa, dbo.fa_notaCreDeb.no_fecha, P.pe_nombreCompleto AS Nombres, 
-                  SUM(dbo.fa_notaCreDeb_det.sc_subtotal) AS sc_subtotal, SUM(dbo.fa_notaCreDeb_det.sc_iva) AS sc_iva, SUM(dbo.fa_notaCreDeb_det.sc_total) AS sc_total, dbo.fa_notaCreDeb.Estado, dbo.fa_notaCreDeb.CodNota, 
+                  CAST(fa_notaCreDeb_resumen.SubtotalConDscto AS FLOAT) AS sc_subtotal, CAST(fa_notaCreDeb_resumen.ValorIVA AS FLOAT) AS sc_iva, CAST(fa_notaCreDeb_resumen.Total AS FLOAT) AS sc_total, dbo.fa_notaCreDeb.Estado, dbo.fa_notaCreDeb.CodNota, 
                   dbo.fa_notaCreDeb.Fecha_Autorizacion, dbo.fa_notaCreDeb.NumAutorizacion, dbo.fa_notaCreDeb.NaturalezaNota, dbo.fa_notaCreDeb_x_ct_cbtecble.ct_IdTipoCbte, dbo.fa_notaCreDeb_x_ct_cbtecble.ct_IdCbteCble
-FROM     dbo.fa_notaCreDeb INNER JOIN
-                  dbo.fa_cliente_contactos ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_cliente_contactos.IdEmpresa AND dbo.fa_notaCreDeb.IdCliente = dbo.fa_cliente_contactos.IdCliente LEFT OUTER JOIN
-                  dbo.fa_notaCreDeb_det ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_notaCreDeb_det.IdEmpresa AND dbo.fa_notaCreDeb.IdSucursal = dbo.fa_notaCreDeb_det.IdSucursal AND 
-                  dbo.fa_notaCreDeb.IdBodega = dbo.fa_notaCreDeb_det.IdBodega AND dbo.fa_notaCreDeb.IdNota = dbo.fa_notaCreDeb_det.IdNota INNER JOIN
+FROM     dbo.fa_notaCreDeb LEFT OUTER JOIN
+                  dbo.fa_notaCreDeb_resumen ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_notaCreDeb_resumen.IdEmpresa AND dbo.fa_notaCreDeb.IdSucursal = dbo.fa_notaCreDeb_resumen.IdSucursal AND 
+                  dbo.fa_notaCreDeb.IdBodega = dbo.fa_notaCreDeb_resumen.IdBodega AND dbo.fa_notaCreDeb.IdNota = dbo.fa_notaCreDeb_resumen.IdNota INNER JOIN
                   dbo.fa_cliente AS C ON C.IdEmpresa = dbo.fa_notaCreDeb.IdEmpresa AND C.IdCliente = dbo.fa_notaCreDeb.IdCliente INNER JOIN
                   dbo.tb_persona AS P ON P.IdPersona = C.IdPersona LEFT OUTER JOIN
                   dbo.fa_notaCreDeb_x_ct_cbtecble ON dbo.fa_notaCreDeb.IdEmpresa = dbo.fa_notaCreDeb_x_ct_cbtecble.no_IdEmpresa AND dbo.fa_notaCreDeb.IdSucursal = dbo.fa_notaCreDeb_x_ct_cbtecble.no_IdSucursal AND 
                   dbo.fa_notaCreDeb.IdBodega = dbo.fa_notaCreDeb_x_ct_cbtecble.no_IdBodega AND dbo.fa_notaCreDeb.IdNota = dbo.fa_notaCreDeb_x_ct_cbtecble.no_IdNota
-GROUP BY dbo.fa_notaCreDeb.IdEmpresa, dbo.fa_notaCreDeb.IdSucursal, dbo.fa_notaCreDeb.IdBodega, dbo.fa_notaCreDeb.IdNota, dbo.fa_notaCreDeb.CreDeb, dbo.fa_notaCreDeb.Serie1, dbo.fa_notaCreDeb.Serie2, 
-                  dbo.fa_notaCreDeb.NumNota_Impresa, dbo.fa_notaCreDeb.no_fecha, P.pe_nombreCompleto, dbo.fa_notaCreDeb.Estado, dbo.fa_notaCreDeb.CodNota, dbo.fa_notaCreDeb.Fecha_Autorizacion, dbo.fa_notaCreDeb.NumAutorizacion, 
-                  dbo.fa_notaCreDeb.NaturalezaNota, dbo.fa_notaCreDeb_x_ct_cbtecble.ct_IdTipoCbte, dbo.fa_notaCreDeb_x_ct_cbtecble.ct_IdCbteCble
 
---SELECT * FROM [Facturacion_electronica_offline].DBO.tb_Comprobante WHERE IdComprobante LIKE '%NC-001-002-000002652%'
-
---SELECT * FROM fa_notaCreDeb where NumNota_Impresa = '000002652'
+--where fa_notaCreDeb.NumNota_Impresa = 2746
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'web', @level1type = N'VIEW', @level1name = N'vwfa_notaCreDeb';
 
