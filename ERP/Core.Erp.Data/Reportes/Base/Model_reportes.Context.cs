6d31fb6d03909845384e5bfc18e9552c3686cc0d
Jorge Aroca <jaroca@fixed-sa.com>
2020-01-21 14:03:52 -0500

    .

--- ERP/Core.Erp.Data/Reportes/Base/Model_reportes.Context.cs
+++ ERP/Core.Erp.Data/Reportes/Base/Model_reportes.Context.cs
@@ -137,6 +137,7 @@ namespace Core.Erp.Data.Reportes.Base
         public virtual DbSet<VWBAN_003> VWBAN_003 { get; set; }
         public virtual DbSet<VWCXP_001> VWCXP_001 { get; set; }
         public virtual DbSet<VWFAC_007> VWFAC_007 { get; set; }
+        public virtual DbSet<VWCONTA_013> VWCONTA_013 { get; set; }
     
         public virtual ObjectResult<SPACTF_004_detalle_Result> SPACTF_004_detalle(Nullable<int> idEmpresa, Nullable<System.DateTime> fecha_corte, string idUsuario, Nullable<int> idActivoFijoTipo_ini, Nullable<int> idActivoFijoTipo_fin, Nullable<int> idCategoria_ini, Nullable<int> idCategoria_fin, string estado_Proceso)
         {
---
+++ ERP_database/ERP_database/web/Views/VWCONTA_013.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [web].[VWCONTA_013]
+AS
+SELECT        dbo.ct_cbtecble_det.IdEmpresa, dbo.ct_cbtecble_det.IdTipoCbte, dbo.ct_cbtecble_det.IdCbteCble, dbo.ct_cbtecble_det.secuencia, dbo.ct_cbtecble_det.IdCtaCble, dbo.ct_plancta.pc_Cuenta, dbo.ct_cbtecble_det.dc_Valor, 
+                         dbo.ct_cbtecble.cb_Observacion, dbo.ct_cbtecble_det.IdPunto_cargo_grupo, dbo.ct_cbtecble_det.IdPunto_cargo, dbo.ct_punto_cargo.nom_punto_cargo, dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo, 
+                         dbo.ct_cbtecble.cb_Fecha, dbo.ct_cbtecble_tipo.tc_TipoCbte, '[' + dbo.ct_cbtecble_det.IdCtaCble + '] ' + dbo.ct_plancta.pc_Cuenta AS TituloGrupo, 'TOTAL ' + dbo.ct_plancta.pc_Cuenta AS TituloTotalGrupo, 
+                         '[' + ISNULL(CAST(dbo.ct_cbtecble_det.IdPunto_cargo_grupo AS varchar), '') + '] ' + dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo AS nom_punto_cargo_grupoFiltro, 'TOTAL '+dbo.ct_punto_cargo_grupo.nom_punto_cargo_grupo TotalFinal
+FROM            dbo.ct_cbtecble_det INNER JOIN
+                         dbo.ct_cbtecble ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_cbtecble.IdEmpresa AND dbo.ct_cbtecble_det.IdTipoCbte = dbo.ct_cbtecble.IdTipoCbte AND dbo.ct_cbtecble_det.IdCbteCble = dbo.ct_cbtecble.IdCbteCble INNER JOIN
+                         dbo.ct_plancta ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_plancta.IdEmpresa AND dbo.ct_cbtecble_det.IdCtaCble = dbo.ct_plancta.IdCtaCble INNER JOIN
+                         dbo.ct_cbtecble_tipo ON dbo.ct_cbtecble.IdEmpresa = dbo.ct_cbtecble_tipo.IdEmpresa AND dbo.ct_cbtecble.IdTipoCbte = dbo.ct_cbtecble_tipo.IdTipoCbte LEFT OUTER JOIN
+                         dbo.ct_punto_cargo ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo.IdEmpresa AND dbo.ct_cbtecble_det.IdPunto_cargo = dbo.ct_punto_cargo.IdPunto_cargo LEFT OUTER JOIN
+                         dbo.ct_punto_cargo_grupo ON dbo.ct_cbtecble_det.IdEmpresa = dbo.ct_punto_cargo_grupo.IdEmpresa AND dbo.ct_cbtecble_det.IdPunto_cargo_grupo = dbo.ct_punto_cargo_grupo.IdPunto_cargo_grupo
+WHERE        (dbo.ct_cbtecble_det.IdPunto_cargo_grupo IS NOT NULL)
