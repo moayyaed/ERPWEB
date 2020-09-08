d757c29e43ddca5aacaf4a0fda9ff4e521218ed9
arocajorge <arocajorge92@gmail.com>
2020-09-08 10:23:02 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Tables/fa_factura_tipo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[fa_factura_tipo] (
+    [IdEmpresa]             INT            NOT NULL,
+    [IdFacturaTipo]         INT            NOT NULL,
+    [Codigo]                VARCHAR (50)   NOT NULL,
+    [Descripcion]           VARCHAR (1000) NOT NULL,
+    [Estado]                BIT            NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200)  NULL,
+    [FechaCreacion]         DATETIME       NULL,
+    [IdUsuarioModificacion] VARCHAR (200)  NULL,
+    [FechaModificacion]     DATETIME       NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200)  NULL,
+    [FechaAnulacion]        DATETIME       NULL,
+    [MotivoAnulacion]       VARCHAR (MAX)  NULL,
+    CONSTRAINT [PK_fa_factura_tipo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdFacturaTipo] ASC)
+);
+
