e4d48fb6eedbd586e2784f301e9705ece82bbf57
arocajorge <arocajorge92@gmail.com>
2020-11-21 10:16:40 -0500

    eeee

--- ERP/Core.Erp.Data/ConexionesERP.cs
+++ ERP/Core.Erp.Data/ConexionesERP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,37 +11,7 @@ namespace Core.Erp.Data
     {
         public static string GetConnectionString()
         {
-            string ip = string.Empty;
-            string password = string.Empty;
-            string user = string.Empty;
-            string InitialCatalog = string.Empty;
-            string Cadena = "GRAFINPREN";
-            switch (Cadena)
-            {
-                case "WEB":
-                    ip = "fixed.database.windows.net";
-                    password = "admin*2016";
-                    user = "administrador";
-                    InitialCatalog = "AgricolaYTransporte";
-                    break;
-
-                case "GRAFINPREN":
-                    ip = "10.100.5.140";
-                    password = "admin*2016";
-                    user = "sa";
-                    InitialCatalog = "DBERP";
-                    break;
-
-                case "LOCAL":
-                    ip = "localhost";
-                    password = "admin*2016";
-                    user = "sa";
-                    InitialCatalog = "DBERPWEB";
-                    break;
-            }
-            
-            
-            return "data source=" + ip + ";initial catalog=" + InitialCatalog + ";user id=" + user + ";password=" + password + ";MultipleActiveResultSets=True;";
+            return ConfigurationManager.ConnectionStrings["ConnectionDB"].ConnectionString;
         }
     }
 }
