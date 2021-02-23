f178effc84701a4e1b22d213012debd1b59871d7
WendyCherres <wendycherres@gmail.com>
2021-02-23 14:58:08 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Tables/cxc_SeguimientoCartera.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[cxc_SeguimientoCartera] (
+    [IdEmpresa]             INT           NOT NULL,
+    [IdSeguimiento]         INT           NOT NULL,
+    [IdCliente]             NUMERIC (18)  NOT NULL,
+    [CorreoEnviado]         BIT           NOT NULL,
+    [Fecha]                 DATETIME      NOT NULL,
+    [Observacion]           VARCHAR (MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Estado]                BIT           NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [FechaAnulacion]        DATETIME      NULL,
+    [MotivoAnulacion]       VARCHAR (MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    CONSTRAINT [PK_cxc_SeguimientoCartera] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdSeguimiento] ASC),
+    CONSTRAINT [FK_cxc_SeguimientoCartera_fa_cliente] FOREIGN KEY ([IdEmpresa], [IdCliente]) REFERENCES [dbo].[fa_cliente] ([IdEmpresa], [IdCliente])
+);
+
---
+++ ERP_database/ERP_database/dbo/Tables/tb_ColaCorreoParametros.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tb_ColaCorreoParametros] (
+    [IdEmpresa]             INT           NOT NULL,
+    [Usuario]               VARCHAR (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Contrasenia]           VARCHAR (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Puerto]                INT           NOT NULL,
+    [Host]                  VARCHAR (500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [PermitirSSL]           BIT           NOT NULL,
+    [CorreoCopia]           VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    CONSTRAINT [PK_tb_ColaCorreoParametros] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC)
+);
+
