7b4206e213750589c4bb0f234c0eb11048532b90
carloscede <carloscede@hotmail.es>
2021-09-14 21:57:07 +0100

    biometrico

--- ERP/Core.Erp.Info/RRHH/ro_biometrico_Info.cs
+++ ERP/Core.Erp.Info/RRHH/ro_biometrico_Info.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,29 @@ namespace Core.Erp.Info.RRHH
         public int IdEmpresa { get; set; }
         public int IdBiometrico { get; set; }
         public int IdEquipo { get; set; }
+        [Required(ErrorMessage = "El campo descripción es obligatorio")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 100")]
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El campo string conexión es obligatorio")]
+        [StringLength(500, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 500")]
         public string StringConexion { get; set; }
+        [Required(ErrorMessage = "El campo consulta es obligatorio")]
         public string Consulta { get; set; }
         public bool MarcacionIngreso { get; set; }
         public bool MarcacionSalida { get; set; }
         public bool SalidaLounch { get; set; }
         public bool RegresoLounch { get; set; }
+        [Required(ErrorMessage = "El campo codigo marcacion ingreso es obligatorio")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 50")]
         public string CodMarcacionIngreso { get; set; }
+        [Required(ErrorMessage = "El campo codigo marcacion salida es obligatorio")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 50")]
         public string CodMarcacionSalida { get; set; }
+        [Required(ErrorMessage = "El campo codigo marcacion salida lountch es obligatorio")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 50")]
         public string CodSalidaLounch { get; set; }
+        [Required(ErrorMessage = "El campo codigo marcacion regreso lountch es obligatorio")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "La descripción debe tener mínimo 4 caracteres y máximo 50")]
         public string CodRegresoLounch { get; set; }
         public Nullable<bool> Estado { get; set; }
         public string IdUsuario { get; set; }
